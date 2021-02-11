6716a463b86f16b170242c5eb929f7198f6a7b5f
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-02-11 18:09:10 +0200

    Commandline runner and Repository

--- src/main/java/com/example/demo/BookstoreApplication.java
+++ src/main/java/com/example/demo/BookstoreApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+	    return (args) -> {
+		
+	    };
 	}
 
 }
