b7e415d1f36256854b95c93fff878c72486f7dd0
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-03-15 21:04:04 +0200

    removed books from cli

--- src/main/java/com/example/demo/BookstoreApplication.java
+++ src/main/java/com/example/demo/BookstoreApplication.java
@@ -32,9 +32,7 @@ public class BookstoreApplication {
 		User user1 = new User("user", "$2a$10$U0021easazMrjG1E2VfKe.ptEgHYSW6RuGTnJFPFAUrBHTXaJUNwC", "USER", "user@user.com");
 		User user2 = new User("admin", "$2a$10$LWRNHeTjf/5FryyyIsUbKezOH1qqqOfJ45smIBjMcpeYP.FmWHscC", "ADMIN", "admin@admin.com");
 		
-		Book book1 = new Book("Hyvä kirja", "Janne", "35353-2", 2004, category1);
-		Book book2 = new Book("Huono kirja", "Kalle", "45645-2", 1550, category2);
-		Book book3 = new Book("Iha jees kirja", "Maarit", "62623-2", 1890, category3);
+
 		uRep.deleteAll();
 		uRep.save(user1);
 		uRep.save(user2);
@@ -43,9 +41,6 @@ public class BookstoreApplication {
 		cRep.save(category2);
 		cRep.save(category3);
 		
-		bRep.save(book1);
-		bRep.save(book2);
-		bRep.save(book3);
 		
 
 	    };
